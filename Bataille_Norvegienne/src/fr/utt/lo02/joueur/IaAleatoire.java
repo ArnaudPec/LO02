30d63d176f205d9230939dc8349b8e15d3af6f2a
ArnaudPec <arnaud.pecoraro@utt.fr>
2014-11-19 18:10:22 +0100

    Test d'envoi : Ajout des classes filles de IA

---
+++ Bataille_Norvegienne/src/fr/utt/lo02/joueur/IaAleatoire.java
@@ -0,0 +1,9 @@
+package fr.utt.lo02.joueur;
+
+public class IaAleatoire extends IA {
+
+	public IaAleatoire() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Bataille_Norvegienne/src/fr/utt/lo02/joueur/IaOffensive.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package fr.utt.lo02.joueur;
+
+/**
+ * @author arnaud
+ *
+ */
+public class IaOffensive extends IA {
+
+	/**
+	 * 
+	 */
+	public IaOffensive() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
